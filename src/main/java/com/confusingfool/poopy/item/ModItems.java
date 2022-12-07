bd4aace6822a8a8c8ba60533c31081443174fd97
Uriasej <uriasej@owensborocatholic.us>
2022-12-07 00:07:56 -0600

    Added Dried Poopy
    You can eat it to get Resistence V
    
    Smoke Poopy to get Dried Poopy and 10 xp
    
    Gonna add in Fermented Poopy that gives you confusion and strength.

--- src/main/java/com/confusingfool/poopy/item/ModItems.java
+++ src/main/java/com/confusingfool/poopy/item/ModItems.java
@@ -1,6 +1,10 @@
 package com.confusingfool.poopy.item;
 
 import com.confusingfool.poopy.Poopy;
+import net.minecraft.client.renderer.EffectInstance;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,6 +22,9 @@ public class ModItems
 
     public static final RegistryObject<Item> POOPY_DIAMOND = ITEMS.register("poopy_diamond",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.POOPY_TAB)));
+
+    public static final RegistryObject<Item> DRIED_POOPY = ITEMS.register("dried_poopy",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.POOPY_TAB).food(new FoodProperties.Builder().nutrition(8).saturationMod(6f).meat().fast().effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 6000, 4), 0.9f).build())));
 
 
     public static void register(IEventBus eventBus)
