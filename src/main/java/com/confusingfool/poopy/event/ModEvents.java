85de57e34e179ee95fdea37fc15a8dbbf204d70e
Uriasej <uriasej@owensborocatholic.us>
2022-12-07 22:29:24 -0600

    Added event listener that checks if player is eating meat and crouching and onGround() and spawns poopy on the player for as long as they are eating.
    Changed the Dried Poopy recipe to be 1200 ticks instead of 200 for balancing reasons. I might even make it something like 5 minutes for one Dried Poopy.

--- src/main/java/com/confusingfool/poopy/event/ModEvents.java
+++ src/main/java/com/confusingfool/poopy/event/ModEvents.java
@@ -4,9 +4,13 @@ import com.confusingfool.poopy.Poopy;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.animal.Sheep;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import static com.confusingfool.poopy.item.ModItems.POOPY;
 
 @Mod.EventBusSubscriber(modid = Poopy.MOD_ID)
 public class ModEvents
@@ -19,6 +23,24 @@ public class ModEvents
             if (event.getSource().getEntity() instanceof Player player)
             {
                 player.sendSystemMessage(Component.literal(player.getName().getString() + "hurt a sheep"));
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerEats(LivingEntityUseItemEvent event)
+    {
+        if (event.getEntity() instanceof Player player)
+        {
+            if (event.getItem().getFoodProperties(player).isMeat())
+            {
+                if (player.isCrouching())
+                {
+                    if (player.isOnGround())
+                    {
+                        player.spawnAtLocation(new ItemStack(POOPY.get()));
+                    }
+                }
             }
         }
     }
