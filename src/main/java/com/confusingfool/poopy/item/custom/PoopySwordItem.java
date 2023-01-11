8773871feab04ba2856b6282b30b1cb53be341ef
Uriasej <uriasej@owensborocatholic.us>
2023-01-11 10:26:59 -0600

    Added Poopy Diamond Sword lang
    Updated mods.toml to include Poopy Diamond Sword

--- src/main/java/com/confusingfool/poopy/item/custom/PoopySwordItem.java
+++ src/main/java/com/confusingfool/poopy/item/custom/PoopySwordItem.java
@@ -1,12 +1,19 @@
 package com.confusingfool.poopy.item.custom;
 
 import com.confusingfool.poopy.effect.ModEffects;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
 
 public class PoopySwordItem extends SwordItem
 {
@@ -22,5 +29,21 @@ public class PoopySwordItem extends SwordItem
         target.addEffect(new MobEffectInstance(ModEffects.DYSENTERY.get(), 200), attacker);
 
         return super.hurtEnemy(itemStack, target, attacker);
+    }
+
+
+    @Override
+    public void appendHoverText(ItemStack stack, Level level, List<Component> components, TooltipFlag flag)
+    {
+        if (Screen.hasShiftDown())
+        {
+            components.add(Component.literal("When you hit something, it gets Dysentery.").withStyle(ChatFormatting.AQUA));
+        } else {
+            components.add(Component.literal("Press SHIFT for more info").withStyle(ChatFormatting.YELLOW));
+        }
+
+
+
+        super.appendHoverText(stack, level, components, flag);
     }
 }
